e7f882caa3eec2bde1c362bfdeefeca7bbada95f
KeaweA <keaweaquarian@gmail.com>
2023-10-01 21:51:23 -0400

    demoTest

--- src/test/java/DemoTest.java
+++ src/test/java/DemoTest.java
@@ -79,13 +79,88 @@ public class DemoTest {
         Demo.main(args);
 
     }
+    @Test(expected = java.util.NoSuchElementException.class)
+    public void mainFalseIncomplete() {
+
+        String input = "1\n";
+        input += "2\n";
+
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+        System.setOut(new PrintStream(out));
+
+        String[] args = {};
+        Demo.main(args);
+
+        //(expected) output
+        String consoleOutput = "Enter side 1: " + System.getProperty("line.separator");
+        consoleOutput += "Enter side 2: " + System.getProperty("line.separator");
+        consoleOutput += "Enter side 3: " + System.getProperty("line.separator");
+        consoleOutput += "This is not a triangle." + System.getProperty("line.separator");
+
+        assertEquals(consoleOutput, out.toString() );
+    }
+    @Test(expected = java.util.InputMismatchException.class)
+    public void testMainWithInvalidInput2() {
+        // Provide non-double input to the main method
+        String input = "1\n";
+        input += "bbb\n";
+        input += "3\n";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+        System.setOut(new PrintStream(out));
+
+        String[] args = {};
+        Demo.main(args);
+
+    }
+    @Test(expected = java.util.InputMismatchException.class)
+    public void testMainWithInvalidInput3() {
+        // Provide non-double input to the main method
+        String input = "1\n";
+        input += "2\n";
+        input += "ccc\n";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+        System.setOut(new PrintStream(out));
+
+        String[] args = {};
+        Demo.main(args);
+
+    }
+    @Test(expected = java.util.InputMismatchException.class)
+    public void testMainWithDoubleInout() {
+        // Provide non-double input to the main method
+        String input = "2147483648\n";
+        input += "2147483647\n";
+        input += "2147483647\n";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+        System.setOut(new PrintStream(out));
+
+        String[] args = {};
+        Demo.main(args);
+    }
 
     @Test
     public void isTriangle() {
-
-        assertTrue(Demo.isTriangle(4.0,2.0,3.0));
+        assertTrue(Demo.isTriangle(1,1,1));
+        assertTrue(Demo.isTriangle(5.0F, 12.0F, 13.0F));
+        assertTrue(Demo.isTriangle(1.0,1.0,1.0));
         assertTrue(Demo.isTriangle(5.0, 12.0, 13.0));
         assertTrue(Demo.isTriangle(8.0, 15.0, 17.0));
+        assertTrue(Demo.isTriangle(Double.MAX_VALUE, Double.MAX_VALUE, Double.MAX_VALUE));
+        assertTrue(Demo.isTriangle(Double.MIN_VALUE, Double.MIN_VALUE, Double.MIN_VALUE));
+        assertTrue(Demo.isTriangle(1L,1L,1L));
+
     }
 
     @Test
@@ -94,6 +169,13 @@ public class DemoTest {
         assertFalse(Demo.isTriangle(1.0,2.0,3.0));
         assertFalse(Demo.isTriangle(5.0, 10.0, 25.0));
         assertFalse(Demo.isTriangle(0, 0, 0));
+        assertFalse(Demo.isTriangle(0.5, 1.0,0.5));
+        assertFalse(Demo.isTriangle(1, 0.5, 0.5));
+        assertFalse(Demo.isTriangle(1, 0.5, 0.5));
+        assertFalse(Demo.isTriangle(1L,2L,3L));
+        assertFalse(Demo.isTriangle(1.0F,2.0F,3.0F));
+
+
     }
 
     @Test
@@ -101,6 +183,7 @@ public class DemoTest {
         assertFalse(Demo.isTriangle(-3.0, 4.0, 5.0));
         assertFalse(Demo.isTriangle(3.0, -4.0, 5.0));
         assertFalse(Demo.isTriangle(3.0, 4.0, -5.0));
+        assertFalse(Demo.isTriangle(-3.0, -4.0, -5.0));
     }
 
     @Test
@@ -108,6 +191,7 @@ public class DemoTest {
         assertFalse(Demo.isTriangle(0.0, 4.0, 5.0));
         assertFalse(Demo.isTriangle(3.0, 0.0, 5.0));
         assertFalse(Demo.isTriangle(3.0, 4.0, 0.0));
+        assertFalse(Demo.isTriangle(0.0, 0.0, 0.0));
     }
 
 
